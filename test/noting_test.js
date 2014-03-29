b63eabaf4eb6245a67a06eb59cdfc45974cdd2a7
Grzegorz Lipok <grzes.l@gmail.com>
2014-03-29 12:28:57 +0100

    Corrected include

--- test/noting_test.js
+++ test/noting_test.js
@@ -1,5 +1,5 @@
 var assert = require("assert"),
-do_nothing = require('../index').do_nothing;
+do_nothing = require('../nothing').do_nothing;
 describe('do_nothing', function(){
     it('should return null', function(){
         assert.equal(null, do_nothing());
