896ec238dcfcab00b276171f7ee1d237bd00ef60
Kamil Gałuszka <kgaluszka@nglogic.com>
2014-03-29 11:36:12 +0100

    added test for function add

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var assert = require("assert"),
+    add = require('../index').add;
+describe('add', function(){
+    it('should return addition of 2 numbers', function(){
+        assert.equal(6, add(5,1));
+        assert.equal(8, add(6,2));
+     })
+})
